c64f89d1cdd2f571efee00b81e273492ad5fb3e6
Oruluk <pat.dein@gmail.com>
2021-09-25 20:30:33 +0200

    Design tunings

--- line_client/lib/components/exercising_counter.dart
+++ line_client/lib/components/exercising_counter.dart
@@ -7,7 +7,9 @@ class ExercisingCounter extends StatefulWidget {
   final IconData icon;
   final String mode;
 
-  const ExercisingCounter({ Key? key, required this.icon, this.mode = 'heart_rate' }) : super(key: key);
+  const ExercisingCounter(
+      {Key? key, required this.icon, this.mode = 'heart_rate'})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _ExercisingCounterState();
@@ -25,42 +27,36 @@ class _ExercisingCounterState extends State<ExercisingCounter> {
   @override
   void initState() {
     super.initState();
-    if (widget.mode == 'heart_rate')
-      {
-        generateNextHeartRate();
-      } else {
+    if (widget.mode == 'heart_rate') {
+      updateHeartRateLabel(last);
+      generateNextHeartRate();
+    } else {
       updateCountLabel();
     }
   }
-
 
   void setLabel(String text) {
     this.label = text;
   }
 
-
-  void updateCountLabel()
-  {
+  void updateCountLabel() {
     setState(() {
       this.label = this.count.toString() + '/5';
     });
   }
 
-
-  void generateNextHeartRate()
-  {
-      {
-        Future.delayed(const Duration(milliseconds: 1500), () {
-          last = min(maxRate, max(minRate, last + steps[rnd.nextInt(steps.length - 1)]));
-          updateHeartRateLabel(last);
-          generateNextHeartRate();
-        });
-      }
+  void generateNextHeartRate() {
+    {
+      Future.delayed(const Duration(milliseconds: 1500), () {
+        last = min(
+            maxRate, max(minRate, last + steps[rnd.nextInt(steps.length - 1)]));
+        updateHeartRateLabel(last);
+        generateNextHeartRate();
+      });
+    }
   }
 
-
-  void updateHeartRateLabel(int rate)
-  {
+  void updateHeartRateLabel(int rate) {
     setState(() {
       this.label = rate.toString();
     });
@@ -69,51 +65,45 @@ class _ExercisingCounterState extends State<ExercisingCounter> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(bottom: 8, top: 4),
-      child: new GestureDetector(
+        padding: EdgeInsets.only(bottom: 8, top: 4),
+        child: new GestureDetector(
             child: new Container(
-            height: 50.0,
-            width: 100.0,
-            color: Colors.transparent,
-            child: new Container(
-            decoration: new BoxDecoration(
-                color: new Color(0xAB7487FF),
-                borderRadius: new BorderRadius.all(
-                    Radius.circular(20.0)
+                height: 50.0,
+                width: 100.0,
+                color: Colors.transparent,
+                child: new Container(
+                  decoration: new BoxDecoration(
+                      color: new Color(0xFF4C8BF5),
+                      borderRadius:
+                          new BorderRadius.all(Radius.circular(20.0))),
+                  child: new Center(
+                    child: new Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              widget.icon,
+                              size: 24.0,
+                              color: Colors.white
+                            ),
+                            Text(
+                              this.label,
+                              style: GoogleFonts.lato(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        )),
+                  ),
                 )),
-            child: new Center(
-              child: new Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        widget.icon,
-                        size: 24.0,
-                      ),
-                      Text(
-                        this.label,
-                        style: GoogleFonts.lato(
-                          color: Colors.black87,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      )
-                    ],
-                  )),
-            ),
-          )
-          ),
-          onTap: (){
-            if (widget.mode == 'count')
-              {
+            onTap: () {
+              if (widget.mode == 'count') {
                 count += 1;
                 updateCountLabel();
               }
-          }
-
-    )
-
-    );
+            }));
   }
 }
